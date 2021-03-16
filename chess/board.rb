770a5768492896965b0d10b1ba626f273d286350
Evan-Leon <evan.leon.j@gmail.com>
2021-03-16 16:19:56 -0400

    Defined populate

--- chess/board.rb
+++ chess/board.rb
@@ -19,16 +19,33 @@ class Board
     end
 
     def populate
-        @grid.each_with_index do |row, idx|
-            if (0..1).include?(idx) || (6..7).include?(idx)
-                row.each_with_index { |space, idx2| space << Piece.new([idx, idx2]) }
-            else
-                row.map { |space| space << nil }
+        
+        @grid.each_with_index do |row, row_idx|
+           row.each_with_index do |col, col_idx| 
+                if (0..1).include?(row_idx) || (6..7).include?(row_idx)
+                    row[row_idx, col_idx] << Piece.new([row_idx, col_idx]) 
+                else
+                 row.map { |space| space = nil }
+                end
             end
         end
     end
 
+    def [](pos)
+        x, y = pos 
+        @grid[x, y]
+    end
 
+    def []=(new_pos, value)
+        
+        @grid[new_pos] = value 
+    end  
+
+    # @grid.each do |row|
+    #     row.each do |ele|
+
+    #     end
+    # end
 
 
     #rows 0 & 1 have pieces
