92a0bf0f3d70ed9a21f0dfa768771c14cb39cb32
Samantha Lilly <samanthalillydesign@gmail.com>
2021-03-16 14:32:06 -0400

    Add Manager class

--- class_inheritance/office.rb
+++ class_inheritance/office.rb
@@ -13,30 +13,64 @@ class Employee
 end
 
 class Manager < Employee
-    attr_reader :employees
-    def initialize
-        super()
+
+    # attr_accessor :employees
+
+    def initialize(name, title, salary, boss)
+        super
         @employees = []
     end
 
+    def add_employee(manager, employee)
+        @employees << employee
+    end
+
+    def employees
+        @employees = []
+    end
+
+
     def bonus(multiplier)
 
-
-        employees.each do |employee|
-
+        quu = [employees]
+        count_sal = 0
+        until quu.empty? 
+            ele = quu.shift 
+            count_sal += ele.salary 
+            ele.employees.each { |employee| quu << employee} 
         end
-    end
-end
 
-def bfs(target)
-    quu = [employees]
-    count_sal = 0
-    until quu.empty? 
-      ele = quu.shift 
-      count_sal += employee.salary 
-      ele.employees.each { |employee| quu << employee} 
+        count_sal * multiplier
 
     end
 
 end
+
+
+ned = Manager.new("Ned", "Founder", 1000000, nil)
+darren = Manager.new("Darren", "TA Manager", 78000, ned)
+shawna = Employee.new("Shawna", "TA", 12000, darren)
+david = Employee.new("David", "TA", 10000, darren)
+
+ned.add_employee(darren)
+darren.add_employee(shawna)
+darren.add_employee(david)
+
+p darren.bonus(4)
+
+
+
+
+
+# def bfs(target)
+#     quu = [employees]
+#     count_sal = 0
+#     until quu.empty? 
+#       ele = quu.shift 
+#       count_sal += employee.salary 
+#       ele.employees.each { |employee| quu << employee} 
+
+#     end
+
+# end
 
